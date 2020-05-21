40c12ec7f37428c8d9a156145965fe87d551d218
AnthiPap <anthi1papa@gmail.com>
2020-05-22 01:45:51 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const axios = require('axios');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res) {
+	res.render("home");
+});
+
+app.get("/failure", function(req,res) {
+	res.render("failure");
+})
+
+app.post("/", function(req,res) {
+	const reqCountry = req.body.country[0].toLowerCase();
+	const covidUrl = "https://api.covid19api.com/total/country/" + reqCountry;
+	axios.get(covidUrl)
+	.then(resp => {
+	const countryData = resp.data[resp.data.length - 1];
+    res.render("city-info", {confirmed: countryData.Confirmed, recovered: countryData.Recovered, active: countryData.Active, deaths: countryData.Deaths});
+})
+	.catch(function (error) {
+        res.redirect("/failure");
+    });
+
+});
+
+app.get("/city-info", function(req,res) {
+	res.render("city-info");
+});
+
+app.listen(3000, function() {
+	console.log("Server started on port 3000");
+});
