57e4d8a450fdb222a4fa8bfc6d120ecde88ffc6d
AnthiPap <anthi1papa@gmail.com>
2020-05-22 01:51:42 +0200

    changed server a bit

--- app.js
+++ app.js
@@ -36,6 +36,10 @@ app.get("/city-info", function(req,res) {
 	res.render("city-info");
 });
 
-app.listen(3000, function() {
-	console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started.");
 });
